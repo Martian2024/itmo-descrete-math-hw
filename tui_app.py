fe0f7e0b8c70b436b7c9f2cb731ca9a62fd3de0f
Arseniy <arseniy.kolomeytsev@gmail.com>
2025-01-12 19:49:03 +0300

    now i consider myself dumb as fuck

--- tui_app.py
+++ tui_app.py
@@ -3,25 +3,46 @@ import sys
 from main import read_file, kosaraju, tarjan
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Добро пожаловать в программу по поиску Компенент Сильной Связности (КСС) \
-                                     при помощи алгоритмов Косарайю и Тарьяна!")
-    parser.add_argument("filename", help="Абсолютный путь к файлу")
-    parser.add_argument("-k", "--kosaraju", help="Использовать алгоритм Косарайю", action='store_true')
-    parser.add_argument("-t", "--tarjan", help="Использовать алгоритм Тарьяна", action='store_true')
-    namespace = parser.parse_args(sys.argv[1:])
-    graph = read_file(namespace.filename)
-    if namespace.tarjan:
+    graph = None
+    while graph == None:
+        try:
+            graph = read_file(input('Введите абсолютный путь к файлу с графом. \nВнимание! Файл должен четко соответствовать формату: 1-я строка - названия вершин через пробел, \nдалее, каждый раз на новой строке, записываются две вершины одного ребра через пробел. '))
+        except FileNotFoundError:
+            print('Данный файл не найден!')
+        except KeyError:
+            print('Файл не соответствует необходимому формату!')
+
+    prefered_algorithm = input('Введите предпочитаемый алгоритм: Косарайю[k], Тарьяна[t] или оба[b]: ')
+    while prefered_algorithm not in ['b', 't', 'k']:
+        print('Проверьте свой ввод!')
+        prefered_algorithm = input('Введите предпочитаемый алгоритм: Косарайю[k], Тарьяна[t] или оба[b]: ')
+
+    print(f'Количество вершин в графе: {len(graph.keys())}, количество ребер: {sum([len(i) for i in graph.values()])}')
+    print()
+
+    if prefered_algorithm == 'b':
         print('Поиск Компонент Сильной Связности (КСС) при помощи алгоритма Тарьяна')
         answer, answer_time = tarjan(graph)
-    else:
+        print('Список компонент связности:', answer)
+        print(f'Время выполнения алгоритма: {answer_time} мс.')
+        print()
+
         print('Поиск Компонент Сильной Связности (КСС) при помощи алгоритма Косарайю')
         answer, answer_time = kosaraju(graph)
-    print('Список компонент связности:', answer)
-    print(f'Количество вершин в графе: {len(graph.keys())}, количество ребер: {sum([len(i) for i in graph.values()])}')
-    print(f'Время выполнения алгоритма: {answer_time} мс.')
+        print('Список компонент связности:', answer)
+        print(f'Время выполнения алгоритма: {answer_time} мс.')
+        print()
+    elif prefered_algorithm == 'k':
+        print('Поиск Компонент Сильной Связности (КСС) при помощи алгоритма Косарайю')
+        answer, answer_time = kosaraju(graph)
+        print('Список компонент связности:', answer)
+        print(f'Время выполнения алгоритма: {answer_time} мс.')
+        print()
+    elif prefered_algorithm == 't':
+        print('Поиск Компонент Сильной Связности (КСС) при помощи алгоритма Тарьяна')
+        answer, answer_time = tarjan(graph)
+        print('Список компонент связности:', answer)
+        print(f'Время выполнения алгоритма: {answer_time} мс.')
+        print()
+    
     input('Для выхода нажмите Enter')
-
-    # if namespace.kosaraju or not any((namespace.kosaraju, namespace.tarjan)):
-    #     print(kosaraju(graph))
-    # elif namespace.tarjan:
-    #     print(tarjan(graph))
